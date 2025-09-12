85146a1dc513a1680a9ed87ff00233896e336995
Tamim <gojo82851@gmail.com>
2025-09-12 18:11:03 +0600

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ const toBool = (value) => value === "true";
 
 // Owner & Bot Details
 global.owner = process.env.OWNER_NUMBER;
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.anitav4 = process.env.BOT_NAME || "SASUKE_BOT";
+global.ownername = process.env.OWNER_NAME || "Minato";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.author = process.env.AUTHOR || "MINATO";
+global.channelname = process.env.CHANNEL_NAME || "DAVID CYRIL";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 
 // Auto-Features
